681f3c8371430cb9f91676570c8ecdd679d30676
Sergio Javier Pascuzzo <vodnik@hotmail.com.ar>
2024-07-28 17:45:59 -0300

    Update vistas.sql

--- Proyecto_2_Pascuzzo/sql_project/database_objects/vistas.sql
+++ Proyecto_2_Pascuzzo/sql_project/database_objects/vistas.sql
@@ -1,1 +1,45 @@
 USE AUTOBATTLER;
+
+--Primer vista:
+-- crear una vista para ver los datos de los usuarios y sus tiempos de partida
+-- y composiciones segun el registro de partidas
+DROP VIEW IF EXISTS vw_date_reg;
+CREATE VIEW vw_date_reg AS
+SELECT
+    r.ID_REG,
+    u.NICK,
+    p.TIEMPO_DE_PARTIDA,
+    e.UNIDADES_COMPRADAS
+FROM
+    REGISTRO_DE_PARTIDA AS r
+    JOIN USUARIO AS u ON r.ID_USER = u.ID_USER
+    JOIN PARTIDA AS p ON r.ID_SALA = p.ID_SALA
+    JOIN ESTADISTICAS AS e ON p.ID_ESTADISTICA = e.ID_ESTADISTICA;
+
+-- probar si vw funciona
+SELECT * FROM vw_date_reg 
+ORDER BY `ID_REG` ASC;
+
+-- Segunda vista:
+-- contar cuantas patidas tiene cada usuario
+
+DROP VIEW IF EXISTS vw_user_count_party;
+CREATE VIEW vw_user_count_party AS
+SELECT
+    u.ID_USER,
+    u.NICK,
+    COUNT(r.ID_REG) AS total_partidas
+FROM
+    USUARIO AS u
+    LEFT JOIN REGISTRO_DE_PARTIDA AS r ON u.ID_USER = r.ID_USER
+GROUP BY
+    u.ID_USER, u.NICK;
+
+    -- probar si vw funciona
+SELECT * FROM vw_user_count_party
+ORDER BY `ID_USER` ASC; 
+
+-- seleccionar los usuarios con 0 partidas (luego los usaremos para utilizar en otros objetos a crear)
+SELECT * FROM vw_user_count_party
+WHERE total_partidas = 0
+ORDER BY `ID_USER` ASC;
