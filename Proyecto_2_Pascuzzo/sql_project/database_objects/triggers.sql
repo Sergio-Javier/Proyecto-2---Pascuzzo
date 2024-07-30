758bfa4303d68604079acd114b9dcf306b48c812
Sergio Javier Pascuzzo <vodnik@hotmail.com.ar>
2024-07-30 19:39:47 +0000

    Pascuzzo

--- Proyecto_2_Pascuzzo/sql_project/database_objects/triggers.sql
+++ Proyecto_2_Pascuzzo/sql_project/database_objects/triggers.sql
@@ -1,8 +1,28 @@
 USE AUTOBATTLER;
-
+-- Crea un registro cuando se ingrese un nuevo usuario
 CREATE TABLE 
     LOG_CAMBIOS (
         id_log          INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+    ,   tabla_afectada  VARCHAR (50)
+    ,   accion          VARCHAR(50)
+    ,   fecha           DATETIME
+    ,   ID_USER         INT
+    ,   usuario         VARCHAR(50)
     );
+
+
+
+DELIMITER //
+
+CREATE TRIGGER after_insert_trigger
+AFTER INSERT ON USUARIO
+FOR EACH ROW
+BEGIN
+    INSERT INTO LOG_CAMBIOS (tabla_afectada, accion, fecha,idcliente,usuario)
+    VALUES ('USUARIO', 'INSERT', NOW() , NEW.ID_USER,USER());
+END //
+
+DELIMITER ;
+
 
 
