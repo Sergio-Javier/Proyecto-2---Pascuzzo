d21df69cce49141d3de06f039bca182b9ecf51f4
Sergio Javier Pascuzzo <vodnik@hotmail.com.ar>
2024-07-31 05:09:42 +0000

    Pascuzzo

--- Proyecto_2_Pascuzzo/sql_project/database_objects/stored_procedures.sql
+++ Proyecto_2_Pascuzzo/sql_project/database_objects/stored_procedures.sql
@@ -5,26 +5,26 @@ DELIMITER //
 
 CREATE PROCEDURE sp_rango_user(IN p_id_user INT, OUT p_nick VARCHAR(100), OUT p_nombre_rango VARCHAR(50))
 BEGIN
-    DECLARE puntos_clasif INT;
+    DECLARE puntaje INT;
 
-    SELECT `NICK`, `PUNTOS_CLASIF` INTO p_nick, puntos_clasif
-    FROM `USUARIO`
-    WHERE `ID_USER` = p_id_user;
+    SELECT u.NICK, u.PUNTOS_CLASIF INTO p_nick, puntaje
+    FROM USUARIO AS u
+    WHERE u.ID_USER = p_id_user;
 
     -- Si el ID_USER no existe en la base de datos
     IF p_nick IS NULL THEN
         SET p_nombre_rango = 'USUARIO NO ENCONTRADO';
     ELSE
         -- Determino los rangos segun los puntos
-        IF puntos_clasif BETWEEN 0 AND 9 THEN
+        IF puntaje BETWEEN 0 AND 9 THEN
             SET p_nombre_rango = 'BRONCE';
-        ELSEIF puntos_clasif BETWEEN 10 AND 29 THEN
+        ELSEIF puntaje BETWEEN 10 AND 29 THEN
             SET p_nombre_rango = 'PLATA';
-        ELSEIF puntos_clasif BETWEEN 30 AND 59 THEN
+        ELSEIF puntaje BETWEEN 30 AND 59 THEN
             SET p_nombre_rango = 'ORO';
-        ELSEIF puntos_clasif BETWEEN 60 AND 89 THEN
+        ELSEIF puntaje BETWEEN 60 AND 89 THEN
             SET p_nombre_rango = 'DIAMANTE';
-        ELSEIF puntos_clasif >= 90 THEN
+        ELSEIF puntaje >= 90 THEN
             SET p_nombre_rango = 'PLATINO';
         END IF;
     END IF;
@@ -33,9 +33,9 @@ END //
 
 DELIMITER ;
 
--- ejemplo, me tiene que dar vodnikjavier bronce
-CALL sp_rango_user(3, @p_nick, @p_nombre_rango);
-SELECT @p_nick AS Nick, 
+-- ejemplo, me tiene que dar vodnikjavier bronce, si pongo un numero mayor a 25 me da el msj
+CALL sp_rango_user(24, @p_nick, @p_nombre_rango);
+SELECT @p_nick AS Nick,
        @p_nombre_rango AS Nombre_Rango;
 
 
