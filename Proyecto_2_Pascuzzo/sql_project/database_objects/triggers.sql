4c126309ae46b56aa9035b9df2d91b0a073757fb
Sergio Javier Pascuzzo <vodnik@hotmail.com.ar>
2024-07-31 12:00:15 -0300

    triggers.sql

--- Proyecto_2_Pascuzzo/sql_project/database_objects/triggers.sql
+++ Proyecto_2_Pascuzzo/sql_project/database_objects/triggers.sql
@@ -21,7 +21,7 @@ CREATE TRIGGER after_insert_trigger
 AFTER INSERT ON USUARIO
 FOR EACH ROW
 BEGIN
-    INSERT INTO LOG_CAMBIOS (tabla_afectada, accion, fecha,idcliente,usuario)
+    INSERT INTO LOG_CAMBIOS (tabla_afectada, accion, fecha, ID_USER, usuario)
     VALUES ('USUARIO', 'INSERT', NOW() , NEW.ID_USER,USER());
 END //
 
