289f64c67719a05f5058a7bd0077d8df8a4f2865
Sergio Javier Pascuzzo <vodnik@hotmail.com.ar>
2024-07-31 18:44:31 -0300

    stored_procedures.sql

--- Proyecto_2_Pascuzzo/sql_project/database_objects/stored_procedures.sql
+++ Proyecto_2_Pascuzzo/sql_project/database_objects/stored_procedures.sql
@@ -1,22 +1,24 @@
 USE AUTOBATTLER;
 
+-- BORRAMOS SI EXISTE LOS PROCEDIMIENTOS
 DROP PROCEDURE IF EXISTS sp_rango_user;
+DROP PROCEDURE IF EXISTS delete_user;
 
 DELIMITER //
-
+-- CREAMOS NUESTRO PRIMER PROCEDIMIENTO
 CREATE PROCEDURE sp_rango_user(IN p_id_user INT, OUT p_nick VARCHAR(100), OUT p_nombre_rango VARCHAR(50))
 BEGIN
     DECLARE puntaje INT;
-
+-- LLEVAMOS LOS DATOS A LAS VARIABLES
     SELECT u.NICK, u.PUNTOS_CLASIF INTO p_nick, puntaje
     FROM USUARIO AS u
     WHERE u.ID_USER = p_id_user;
 
-    -- Si el ID_USER no existe en la base de datos
+-- Si el ID_USER no existe en la base de datos
     IF p_nick IS NULL THEN
         SET p_nombre_rango = 'USUARIO NO ENCONTRADO';
     ELSE
-        -- Determino los rangos segun los puntos
+-- Determino los nombres de rangos segun los puntos a comparar en la variable puntaje
         IF puntaje BETWEEN 0 AND 9 THEN
             SET p_nombre_rango = 'BRONCE';
         ELSEIF puntaje BETWEEN 10 AND 29 THEN
@@ -30,6 +32,30 @@ BEGIN
         END IF;
     END IF;
 
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+-- CREAMOS NUESTRO SEGUNDO PROCEDIMIENTO
+CREATE PROCEDURE delete_user (d_id_user INT)
+BEGIN
+-- DECLARAMOS LAS SIGUIENTES VARIABLES
+	DECLARE existe_id INT;
+	DECLARE id VARCHAR(100);
+-- CORROBORAMOS SI EL ID EXISTE CON LA VARIABLE existe_id
+    SET existe_id = (SELECT count(ID_USER) FROM USUARIO WHERE ID_USER = d_id_user);
+-- SI EXISTE BORRARA EL ID
+    IF existe_id > 0 THEN
+    DELETE FROM USUARIO WHERE ID_USER = d_id_user;
+-- SI LO BORRA, ARROJA MSJ DE EXITO
+    SET id = CONCAT (d_id_user,'','_ID Borrado con Exito');
+    else
+-- SINO BORRA, ARROJA MSJ DE ERROR
+    set id = 'Usuario no encontrado';
+    end if;
+    select id AS Estado_de_borrado;
 END //
 
 DELIMITER ;
